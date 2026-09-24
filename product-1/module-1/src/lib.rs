76632f0b97dd413b68195eb357b3f48dc189ef7f
Silas Rodriguez <silasrodriguez02@gmail.com>
2026-09-24 00:47:59 -0400

    Optional dependency based on feature forwarding a feature

--- product-1/module-1/src/lib.rs
+++ product-1/module-1/src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[cfg(feature = "a")]
+pub fn feat_a() {
+    println!("feature a");
+}
+
+#[cfg(feature = "b")]
+pub fn feat_b() {
+    println!("feature b");
+}
+
+#[cfg(feature = "c")]
+pub fn feat_c() {
+    println!("feature c");
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    #[cfg(feature = "a")]
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn feature_a_works() {
+        feat_a();
+    }
+
+    #[cfg(feature = "b")]
+    #[test]
+    fn feature_b_works() {
+        feat_b();
+    }
+
+    #[cfg(feature = "c")]
+    #[test]
+    fn feature_c_works() {
+        feat_c();
     }
 }
--- product-1/src/main.rs
+++ product-1/src/main.rs
@@ -1,3 +1,24 @@
+#[cfg(all(feature = "a", feature = "c"))]
+compile_error!("Unable to build product-1 with a and c features");
+
+#[cfg(feature = "a")]
+use module_a;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Entering product-1!");
+    #[cfg(feature = "a")]
+    {
+        module_1::feat_a();
+        module_a::feat_x();
+    }
+
+    #[cfg(feature = "b")]
+    {
+        module_1::feat_b();
+    }
+
+    #[cfg(feature = "c")]
+    {
+        module_1::feat_c();
+    }
 }
